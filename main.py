07c5e03d0031de8b83cd941a29ea1633648457b3
Deeshan Sharma <deeshansharma04@gmail.com>
2020-12-09 05:38:39 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,5 @@
+'''
+Author: Deeshan Sharma
+Date Created: December 7, 2020
+Purpose: Purpose of this app is to avoid rewriting the same code again and again and also helpful in saving a lot of time by automating the whole process. In writing blogs for the Blogs page on my portfolio.
+'''
